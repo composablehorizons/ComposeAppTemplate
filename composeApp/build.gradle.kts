d9cf6af8f32aa9cd599ec5e4172a92bfb5aa3715
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-07-07 23:26:22 +0300

    Initial commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(compose.components.uiToolingPreview)
+        }
+    }
+}
